5bf05a71352ee6299287c9582bed09d4d9d018f6
Jean-Marie CHATEAUX <jmc@jimbook.home>
2024-01-27 09:30:19 +0100

    test 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/components/App.jsx</code> and save to test HMR
+          git test
         </p>
       </div>
       <p className="read-the-docs">
