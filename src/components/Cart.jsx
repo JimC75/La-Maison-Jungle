203ad8feebef94f03da4c2d23326add6aadef90c
Jean-Marie CHATEAUX <jmc@jimbook.home>
2024-01-27 10:10:39 +0100

    CSS => SASS then add of Banner + Cart components

---
+++ src/components/Cart.jsx
@@ -0,0 +1,33 @@
+export default function Cart() {
+    const plants = [
+        {
+            "name": "Monstera",
+            "price": 8
+        },
+        {
+            "name": "Lierre",
+            "price": 10
+        },
+        {
+            "name": "Bouquet de Fleurs",
+            "price": 15
+        }
+    ]
+    const cartList = plants.map((p, index) => {
+        return (<li key={index}>
+            {p.name + ' = ' + p.price + '€'}
+        </li>)
+    })
+    let total = 0;
+    plants.forEach((p) => {
+        total += p.price
+    })
+    return (
+        <>
+            <ul>
+                {cartList}
+            </ul>
+            <p>Total = {total}</p>
+        </>
+    )
+}
