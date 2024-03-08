27618d8e3b45f3ec4ed14232a3dc7109ce029b96
Jean-Marie CHATEAUX <jmc@jimbook.home>
2024-03-08 23:53:22 +0100

    relative path update in the build dir

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/La-Maison-Jungle/', // Replace with your GitHub repository name
   plugins: [react()],
+  build: {
+    outDir: 'docs', // GitHub Pages requirement: output directory should be 'docs'
+  },
 })
